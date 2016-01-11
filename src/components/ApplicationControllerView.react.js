4179cde4b35c9e1e45cece782c93f5fb55b95e3b
Pivotal <Suresh@Sureshs-Mac-mini.local>
2016-01-11 14:11:50 +0530

    wip the application

--- src/components/ApplicationControllerView.react.js
+++ src/components/ApplicationControllerView.react.js
@@ -2,11 +2,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 //
+// props : owned by parent component
+// state : owned by component itself and its private to the component itself
+//
 var ApplicationControllerView = React.createClass({
+	getInitialState: function(){
+		return ({
+			totalCount:0
+		});
+	},
 	render:function(){
 		return (
 			<div>
 				<header>{this.props.header}</header>
+				<footer>{this.state.totalCount}</footer>
 			</div>
 		);
 	}
