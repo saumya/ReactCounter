d8161934e4e86d6cf17899364a3f68bac605f172
saumya <2saumyaray@gmail.com>
2016-01-12 00:18:42 +0530

    Removed keyMirror library again. Added the values manually. It works now

--- src/constants/FluxCountConstants.js
+++ src/constants/FluxCountConstants.js
@@ -1,5 +1,5 @@
 //var keyMirror = require('react/lib/keyMirror'); // old, deprecated
-
+/*
 var keyMirror = require('fbjs/lib/keyMirror');
 var FluxCountConstants = keyMirror({
 	COUNTER_ADD: null,
@@ -9,18 +9,18 @@ var FluxCountConstants = keyMirror({
 	SET_SELECTED: null,   // Selects a product option
 	RECEIVE_DATA: null    // Loads our mock data
 });
-
-/*
-// Not working this way, 
-// thought, we do not need 'keyMirror' library
-var FluxCountConstants = {
-	COUNTER_ADD: null,
-	CART_ADD: null,       // Adds item to cart
-	CART_REMOVE: null,    // Remove item from cart
-	CART_VISIBLE: null,   // Shows or hides the cart
-	SET_SELECTED: null,   // Selects a product option
-	RECEIVE_DATA: null    // Loads our mock data
-};
 */
+
+// Seems we do not need 'keyMirror' library
+//
+var FluxCountConstants = {
+	COUNTER_ADD: 'COUNTER_ADD',
+	CART_ADD: 'CART_ADD',       
+	CART_REMOVE: 'CART_REMOVE',    
+	CART_VISIBLE: 'CART_VISIBLE',   
+	SET_SELECTED: 'SET_SELECTED',   
+	RECEIVE_DATA: 'RECEIVE_DATA'    
+};
+
 
 module.exports = FluxCountConstants;
