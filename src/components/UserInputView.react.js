afbcf9077e8da8532e6cced0396eaf10bcb23b4a
saumya <Suresh@Sureshs-Mac-mini.local>
2016-01-11 19:38:46 +0530

    Added Event listener

--- src/components/UserInputView.react.js
+++ src/components/UserInputView.react.js
@@ -3,12 +3,21 @@
 //
 var React = require ('react');
 var UserInputView = React.createClass({
+	addCount:function(){
+		console.log('UserInputView:addCount');
+	},
+	incrementCount:function(dataObj){
+		console.log('UserInputView:incrementCount:dataObj:',dataObj);
+		console.log('UserInputView:incrementCount:this.props.today:',this.props.today);
+	},
 	render:function(){
+		var self = this;
 		return (
 			<div>
 				{this.props.today}
 				<span>
-					<button>Count Today</button>
+					<button type="button" className="addCount" onClick={this.addCount}> + </button>
+					<button type="button" className="addCount" onClick={self.incrementCount.bind(self, this.props.today)}> Add </button>
 				</span>
 			</div>
 		);
