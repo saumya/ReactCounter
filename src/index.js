faea50c53662daec0855aaef45040328ef543bde
Pivotal <Suresh@Sureshs-Mac-mini.local>
2016-01-11 13:46:57 +0530

    Done with the setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 // Application entry
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ApplicationControllerView = require('./components/ApplicationControllerView.react');
-// Render the application
-ReactDOM.render(
-	<div>
-		<div>Ract</div>
-		<ApplicationControllerView></ApplicationControllerView>
-	</div>
-	,document.getElementById('reactApp')
-);
+'use strict';
+(function(){
+	// load CSS and make style from it and add the style to the page
+	require("style!css!../css/general.css");
+	//
+	var React = require('react');
+	var ReactDOM = require('react-dom');
+	var ApplicationControllerView = require('./components/ApplicationControllerView.react');
+	// Render the application
+	ReactDOM.render(
+		<div>
+			<div>Ract</div>
+			<ApplicationControllerView></ApplicationControllerView>
+		</div>
+		,document.getElementById('reactApp')
+	);	
+})();
+
