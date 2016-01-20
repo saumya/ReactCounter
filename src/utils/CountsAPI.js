7469c4f9084a89e71d4233f6f64d94d7b06d5faf
saumya <2saumyaray@gmail.com>
2016-01-21 00:09:55 +0530

    Code cleanup

--- src/utils/CountsAPI.js
+++ src/utils/CountsAPI.js
@@ -3,7 +3,6 @@ var FluxCountActions = require('../actions/FluxCountActions');
 var CountsAPI = {
 	// Load mock product data from localStorage into ProductStore via Action
   getCountData: function() {
-    debugger;
   	console.log('CountsAPI : getCountData :');
     var data = JSON.parse(localStorage.getItem('countData'));
     FluxCountActions.recieveCounts(data);
@@ -13,14 +12,10 @@ var CountsAPI = {
     localStorage.clear();
   },
   setCountData: function(newData){
-    //debugger;
     var data = JSON.parse(localStorage.getItem('countData'));
     var _counts = data[0];
     //var days = _counts.days;
-
-    //_counts.days.push(newData);
-    //var n = days.push(newData);
-    _counts.days = newData;
+    _counts.days = newData; // Just set the new Array
 
     //console.log(data);
     //var s = JSON.stringify(n);
